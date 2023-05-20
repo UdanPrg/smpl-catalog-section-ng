59d04c6ebdfe7645eb55cb1d0e3a00b19375d490
Uriel Arana <udan1718@gmail.com>
2023-05-20 13:29:34 -0600

    Firsts steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'smpl-catalog-section-ng';
+  name = 'Uriel';
+  age = 27;
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled = true;
+  person = {
+    name: 'David',
+    age: 26,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  };
+  toogleButton(){
+    this.btnDisabled = !this.btnDisabled;
+  }
+  increaseAge(){
+    this.person.age += 1;
+  }
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop)
+  }
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
